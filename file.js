08f50849565c5f64e85c8b7f818b02cf9c2a59d0
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-01-25 23:10:43 +0100

    First commit

---
+++ file.js
@@ -0,0 +1,51 @@
+var vm = require("vm");
+var $ = {
+  global: Function("return this")(),
+  createRealm(options) {
+    options = options || {};
+    options.globals = options.globals || {};
+    context = { console: console, require: require };
+    for (var glob in options.globals) {
+      context[glob] = options.globals[glob];
+    }
+    var context = vm.createContext(context);
+    vm.runInContext(this.source, context);
+    context.$.source = this.source;
+    context.$.context = context;
+    context.$.destroy = function() {
+      if (options.destroy) {
+        options.destroy();
+      }
+    };
+    return context.$;
+  },
+  evalScript(code) {
+    try {
+      if (this.context) {
+        vm.runInContext(code, this.context, { displayErrors: false });
+      } else {
+        vm.runInESHostContext(code, { displayErrors: false });
+      }
+      return { type: "normal", value: undefined };
+    } catch (e) {
+      return { type: "throw", value: e };
+    }
+  },
+  getGlobal(name) {
+    return this.global[name];
+  },
+  setGlobal(name, value) {
+    this.global[name] = value;
+  },
+  destroy() {},
+  IsHTMLDDA() {
+    return {};
+  },
+  source:
+    "var vm = require('vm');var $ = { global: Function('return this')(), createRealm(options) { options = options || {}; options.globals = options.globals || {}; context = { console: console, require: require }; for(var glob in options.globals) { context[glob] = options.globals[glob]; } var context = vm.createContext(context); vm.runInContext(this.source, context); context.$.source = this.source; context.$.context = context; context.$.destroy = function () { if (options.destroy) { options.destroy(); } }; return context.$; }, evalScript(code) { try { if (this.context) { vm.runInContext(code, this.context, {displayErrors: false}); } else { vm.runInESHostContext(code, {displayErrors: false}); } return { type: 'normal', value: undefined }; } catch (e) { return { type: 'throw', value: e }; } }, getGlobal(name) { return this.global[name]; }, setGlobal(name, value) { this.global[name] = value; }, destroy() {}, IsHTMLDDA() { return {}; }, source: \"\"};function print() { console.log.apply(console, arguments); }"
+};
+function print() {
+  console.log.apply(console, arguments);
+}
+print(1 + 1);
+$.destroy();
